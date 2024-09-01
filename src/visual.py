9e2ea82c7cf586567082add2d6889a8789993423
emryscv <emryscv@gmail.com>
2024-09-01 18:23:37 -0400

    feat/delete and add buttons

--- src/visual.py
+++ src/visual.py
@@ -1,8 +1,10 @@
 import streamlit as st
 import dealer
 
+
 def delete(item):
     st.session_state.user_ratings.pop(item)
+
 
 def find(term, item):
     term = term.lower()
@@ -44,15 +46,15 @@ st.write("Filtro de Categorias. Danos un poco de información sobre tus preferen
 
 colL, colR = st.columns(2, gap="large")
 
-col1, col2 = colL.columns(2)
+colL1, colL2 = colL.columns(2)
 
 filtered_categories = {}
 
 for id, (cat, values) in enumerate(categories.items()):
     if id % 2 == 0:
-        filtered_categories[cat] = col1.multiselect(cat, values)
+        filtered_categories[cat] = colL1.multiselect(cat, values)
     else:
-        filtered_categories[cat] = col2.multiselect(cat, values)
+        filtered_categories[cat] = colL2.multiselect(cat, values)
 
 max_price = colL.slider("Precio maximo", 0, dealer.get_max_price(), 60)
 filtered_categories["Price"] = max_price
@@ -65,26 +67,41 @@ search_term = colR.text_input("Valora los items que has comprado (0-5).")
 # Filtrar los items basados en el término de búsqueda
 filtered_items = [to_str(item) for item in items_list if find(search_term, item)]
 
+colR1, colR2 = colR.columns([0.8, 0.2])
 # Crear un widget de selección múltiple para los items filtrados
-selected_items = colR.multiselect("Selecciona los items comprados", filtered_items)
-
+selected_items = []
+selected_item = colR1.selectbox("Selecciona los items comprados", filtered_items)
+if colR2.button("Add"):
+    selected_items.append(selected_item)
+    
 # Actualizar el estado de la sesión con las nuevas selecciones y sus valoraciones
 for item in selected_items:
     if item not in st.session_state.user_ratings:
         st.session_state.user_ratings[item] = 3  # Valoración por defecto
 
+items_to_delete = []
+
 # Mostrar sliders para todos los items seleccionados y permitir que el usuario les dé una valoración
 if st.session_state.user_ratings:
     colR.write("Items seleccionados y valoraciones:")
-    for item in st.session_state.user_ratings:
-        st.session_state.user_ratings[item] = colR.slider(
+    cols = {}
+    for index, item in enumerate(st.session_state.user_ratings):
+        cols[index] = colR.columns([0.9, 0.1])
+        st.session_state.user_ratings[item] = cols[index][0].slider(
             f"Item: {item}", 1, 5, st.session_state.user_ratings[item]
         )
-        
+        if cols[index][1].button("X", item):
+            items_to_delete.append(item)
 
+for item in items_to_delete:
+    st.session_state.user_ratings.pop(item)
+    if item in selected_items:
+        selected_items.remove(item)
 # Botón para obtener recomendaciones
 if st.button("Obtener Recomendaciones"):
-    recommendations = dealer.get_recomendations(filtered_categories, st.session_state.user_ratings)
+    recommendations = dealer.get_recomendations(
+        filtered_categories, st.session_state.user_ratings
+    )
     st.write("Items recomendados:")
     for item, score in recommendations.items():
         st.write(f"Item: {item}, Puntuación: {score}")
